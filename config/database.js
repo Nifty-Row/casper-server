c37a683843d31045cb5af2ce04b56128aa3893b8
OkekeChristian24 <christian.okeke24@gmail.com>
2023-06-02 07:17:02 -0700

    update prod db values

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
+  HOST: "127.0.0.1",
+  USER: "casper2",
+  PASSWORD: "cAsper@#pas$",
   PORT: 3306,
   DB: "yasuke_casper",
   dialect: "mysql",
