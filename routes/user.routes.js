b0ad7dc0668ac2d643e0d3f6e3c3093d7c24d4fd
Samuel John <58958847+JayEs23@users.noreply.github.com>
2023-08-01 15:22:14 +0100

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,7 @@ const userController = require("../controllers/user.controller");
 const router = express.Router();
 
 router.post("/addNewWallet", userController.addNewWallet);
+router.put("/addNewWallet", userController.addNewWallet);
 router.get("/allUsers", userController.getAllUsers);
 router.get("/usersByCategory/:category", userController.getUsersByCategory);
 router.get("/userByKey/:publicKey", userController.getUserByKey);
