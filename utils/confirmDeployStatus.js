cf6059e551418b6d7ad1a5b2de5b17a45f23f98b
OkekeChristian24 <christian.okeke24@gmail.com>
2023-08-20 11:56:31 -0700

    confirm deploy status update

--- utils/confirmDeployStatus.js
+++ utils/confirmDeployStatus.js
@@ -13,29 +13,35 @@ const NODE_URL = "https://rpc.testnet.casperlabs.io/rpc";
 
 async function confirmDeployStatus(deployHash) {
   const client = new CasperClient(NODE_URL);
-  let i = 300;
-  while (i != 0) {
-    const [deploy, raw] = await client.getDeploy(deployHash);
-    // console.info("exec res: ", raw.execution_results);
-    if (raw.execution_results.length !== 0) {
-      const result = raw.execution_results[0].result;
-      return result;
-
-      // if (raw.execution_results[0].result.Success) {
-      //   return deploy;
-      // } else {
-      //   throw Error(
-      //     "Contract execution: " +
-      //       raw.execution_results[0].result.Failure.error_message
-      //   );
-      // }
-    } else {
-      i--;
-      await sleep(1000);
-      continue;
-    }
+  if (raw.execution_results.length !== 0) {
+    const result = raw.execution_results[0].result;
+    return result;
+  } else {
+    return "pending";
   }
-  throw Error("Timeout after " + i + "s. Something's wrong");
+
+  // let i = 300;
+  // while (i != 0) {
+  //   const [deploy, raw] = await client.getDeploy(deployHash);
+  //   if (raw.execution_results.length !== 0) {
+  //     const result = raw.execution_results[0].result;
+  //     return result;
+
+  //     // if (raw.execution_results[0].result.Success) {
+  //     //   return deploy;
+  //     // } else {
+  //     //   throw Error(
+  //     //     "Contract execution: " +
+  //     //       raw.execution_results[0].result.Failure.error_message
+  //     //   );
+  //     // }
+  //   } else {
+  //     i--;
+  //     await sleep(1000);
+  //     continue;
+  //   }
+  // }
+  // throw Error("Timeout after " + i + "s. Something's wrong");
 }
 
 module.exports = confirmDeployStatus;
